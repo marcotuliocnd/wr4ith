32d2e1fb2560f1fb28fceaffade96245b1a8f960
Marco Túlio Candeo <marcotulionote@gmail.com>
2021-02-09 12:18:41 -0300

    updates

--- bot/index.js
+++ bot/index.js
@@ -1,10 +1,12 @@
 const TwitchClient = require('./src/config/twitch');
+const Server = require('./src/config/server');
 const COMMANDS_SWITCHER = require('./src/config/commands');
 
 require('dotenv').config();
 
 async function bootstrap() {
     await TwitchClient.connect();
+    Server.listen(process.env.PORT || 9999);
     console.log('> Bot connected')
 
     TwitchClient.on('message', messageCallback)
