b60ebb13d573d9ea507a5bca743c5a49b9167f21
Marco Tulio <marcotulionote@gmail.com>
2021-02-26 06:14:34 -0300

    added port to status page

--- bot/src/config/server.js
+++ bot/src/config/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Running');
+    return res.send(`Running on port ${process.env.PORT}`);
 });
 
 module.exports = app;
